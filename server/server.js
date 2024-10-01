803f05be0d8a166eaff8cf020e719241d0d395b8
vmertens <vmertens@travelers.com>
2024-10-01 16:55:49 -0400

    Added routes for /characters and /characters/id

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 const MONGO_URL = process.env.MONGO_DB_URL; 
-// const mongodb = MongoClient(mongoURL); 
+const DB_NAME = process.env.MONGO_DB;  
 
 
 app.get('/', (_, res) => {
@@ -19,6 +19,37 @@ app.get('/api/planets', (_, res) => {
     res.send('Planets')
 })
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}.`)
+app.get('/api/characters/:id', async (req, res)=> { 
+    const characterId = Number(req.params.id); 
+    console.log("In get characters by id route:", characterId);
+    try{
+    const client = await MongoClient.connect(MONGO_URL);
+    const db = client.db(DB_NAME);
+    const collection = db.collection('characters');
+    const characters = await collection.find({"id" : characterId}).toArray();
+    res.json(characters);
+    } catch (err) { 
+        console.log("Error on GET /api/characters: ", err);
+        res.sendStatus(500); 
+    }
+})
+
+app.get('/api/characters', async (_, res)=> { 
+    try{
+    const client = await MongoClient.connect(MONGO_URL);
+    const db = client.db(DB_NAME);
+    const collection = db.collection('characters');
+    const characters = await collection.find({}).toArray();
+    console.log("CHARACTERS:" , collection);
+    res.json(characters);
+    } catch (err) { 
+        console.log("Error on GET /api/characters: ", err);
+        res.sendStatus(500); 
+    }
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}.`)
 })
