6c3975934c5b8a7691f997d356014a8ea73a7327
Benjamin Nguyen <bnguyen4@travelers.com>
2024-10-02 13:41:19 -0400

    add part 5

--- server/server.js
+++ server/server.js
@@ -19,10 +19,7 @@ const collections = {
 };
 
 app.use(cors());
-
-app.get("/", (_, res) => {
-  res.send("Hello world.");
-});
+app.use(express.static('./public'));
 
 // Helper function to connect to the database
 const connectDB = async () => {
